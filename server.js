7b6ed4af3f187d707fa1b3975d3e8fee899c27f5
Anita Lipsky <anita@purplebugs.com>
2022-09-20 13:04:57 +0200

    replace axios with fetch

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const dotenv = require("dotenv").config();
-const express = require("express");
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+const config = dotenv.config();
+import express from "express";
+
 const app = express();
 const port = 3000;
 
@@ -17,11 +20,6 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-var axios = require("axios");
-
-// NB: There is also a Directions API https://developers.google.com/maps/documentation/directions/get-directions
-// eg: https://maps.googleapis.com/maps/api/directions/json?origin=Boston%2C%20MA&destination=Concord%2C%20MA&waypoints=via%3ACharlestown%2CMA%7Cvia%3ALexington%2CMA&key=YOUR_API_KEY'
-
 // Ref: https://developers.google.com/maps/documentation/distance-matrix?hl=en_GB
 // Ref: https://developers.google.com/maps/documentation/distance-matrix/start
 
@@ -29,19 +27,50 @@ const searchParamsDistanceMatrix = new URLSearchParams();
 searchParamsDistanceMatrix.set("origins", "Washington DC");
 searchParamsDistanceMatrix.set("destinations", "New York City");
 searchParamsDistanceMatrix.set("key", process.env.GOOGLE_MAPS_API_KEY);
-const searchParamsDistanceMatrixPath = "distancematrix";
 
-var distanceMatrixAPI = {
-  method: "get",
-  url: `https://maps.googleapis.com/maps/api/${searchParamsDistanceMatrixPath}/json?${searchParamsDistanceMatrix}`,
-  headers: {},
+const distanceMatrixRequest = `https://maps.googleapis.com/maps/api/distancematrix/json?${searchParamsDistanceMatrix}`;
+
+// Ref: https://www.npmjs.com/package/node-fetch#handling-exceptions
+class HTTPResponseError extends Error {
+  constructor(response, ...args) {
+    super(
+      `HTTP Error Response: ${response.status} ${response.statusText}`,
+      ...args
+    );
+    this.response = response;
+  }
+}
+
+const checkStatus = (response) => {
+  if (response.ok) {
+    // response.status >= 200 && response.status < 300
+    console.log("-------- response.ok: distanceMatrixAPI ----------");
+    return response;
+  } else {
+    throw new HTTPResponseError(response);
+  }
 };
 
-axios(distanceMatrixAPI)
-  .then(function (response) {
-    console.log("-------- distanceMatrixAPI ----------");
-    console.log(JSON.stringify(response.data));
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+const response = await fetch(distanceMatrixRequest);
+
+try {
+  const data = await checkStatus(response);
+
+  /*
+  Example response:
+
+  {
+  destination_addresses: [ 'New York, NY, USA' ],
+  origin_addresses: [ 'Washington, DC, USA' ],
+  rows: [ { elements: [Array] } ],
+  status: 'OK'
+  }
+
+  */
+
+  console.log(await data.json());
+} catch (error) {
+  console.error(error);
+  const errorBody = await error.response.text();
+  console.error(`Error body: ${errorBody}`);
+}
