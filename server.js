3623cc58e030985ba9f463220056283c5956a34d
Anita Lipsky <anita@purplebugs.com>
2022-10-06 12:42:46 +0200

    cleanup

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import { Client } from "@elastic/elasticsearch";
 const app = express();
 const port = 3000;
 
+// Ref https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/introduction.html
 const client = new Client({
   cloud: {
     id: process.env.ELASTIC_CLOUD_ID,
@@ -105,50 +106,3 @@ try {
   const errorBody = await error.response.text();
   console.error(`Error body: ${errorBody}`);
 }
-
-// Example use elasticsearch client
-// Ref: https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/introduction.html
-
-async function run() {
-  // Let's start by indexing some data
-  await client.index({
-    index: "game-of-thrones",
-    document: {
-      character: "Ned Stark",
-      quote: "Winter is coming.",
-    },
-  });
-
-  await client.index({
-    index: "game-of-thrones",
-    document: {
-      character: "Daenerys Targaryen",
-      quote: "I am the blood of the dragon.",
-    },
-  });
-
-  await client.index({
-    index: "game-of-thrones",
-    document: {
-      character: "Tyrion Lannister",
-      quote: "A mind needs books like a sword needs a whetstone.",
-    },
-  });
-
-  // here we are forcing an index refresh, otherwise we will not
-  // get any result in the consequent search
-  await client.indices.refresh({ index: "game-of-thrones" });
-
-  // Let's search!
-  const result = await client.search({
-    index: "game-of-thrones",
-    query: {
-      match: { quote: "winter" },
-    },
-  });
-
-  console.log("-------- Elasticsearch quick start: data from cloud ----------");
-  console.log(result.hits.hits);
-}
-
-run().catch(console.log);
